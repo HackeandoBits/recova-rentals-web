fd8dd3b910a0e636394942c6812b655934e7c207
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-10-21 22:48:50 -0300

    refactor:agrega archivos nuevos para el componente del body en el landing-page

--- resources/views/components/application-logo-column.blade.php
+++ resources/views/components/application-logo-column.blade.php
@@ -1,12 +1,14 @@
-<a href="{{ route('gallery') }}" class="flex flex-col items-center justify-center space-y-0.1 group">
+<a href="{{ route('gallery') }}" class="flex flex-col items-center justify-center drop-shadow-sm group">
     <img 
-        src="{{ asset('images/branding/rentalsblanco-1.png') }}" 
+        src="{{ asset('/images/branding/recova-edificio-logo-body.png') }}" 
         alt="Recova Rentals Icon"
-        class="w-auto transition-transform duration-300 group-hover:scale-110"
+        class="w-[512px] transition-transform duration-300 drop-shadow-lg hover:scale-110"
     />
+</a>
+<a href="{{ route('home') }}" class="flex flex-col items-center justify-center drop-shadow-sm group">
     <img 
-        src="{{ asset('images/branding/rentalsblanco-2.png') }}" 
+        src="{{ asset('images/branding/recova-text-logo-body.png') }}" 
         alt="Recova Rentals Wordmark"
-        class=" md:h-auto w-auto transition-all duration-300 group-hover:drop-shadow-[0_0_6px_hsl(310,75%,60%)]"
+        class=" md:h-15 w-[360px] transition-all duration-300 hover:drop-shadow-[0_0_6px_hsl(310,75%,60%)]"
     />
 </a>
