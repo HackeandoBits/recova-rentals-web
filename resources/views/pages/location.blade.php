24346251035bac927f459075b43cbb0f963a7b35
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-10-27 04:42:29 -0300

    feat: agrega card service-area y reorganiza el grid

--- resources/views/pages/location.blade.php
+++ resources/views/pages/location.blade.php
@@ -2,7 +2,7 @@
 @extends('livewire.layout.app')
 
 @section('content')
-<main class="flex gap-5 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pt-14 mb-14">
+<main class="flex gap-5 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-14 mb-14">
      
     {{-- Columna izquierda --}}
     <div class="flex flex-col gap-5 flex-[1]">
@@ -13,16 +13,24 @@
 
         {{-- Card inferior: Contacto --}}
         <div class="bg-[hsl(298,42%,15%)] text-white rounded-lg shadow-lg">
-            @include('sections.contact-section')
+             @include('sections.service-area-section')
+            
         </div>
     </div>
 
-    {{-- Columna derecha: Mapa --}}
-    <div class="flex-[2] flex flex-col">
+    {{-- Columna derecha --}}
+    <div class="flex flex-col gap-5 flex-[2]">
+        {{-- Card superior: Mapa --}}
         <div class="rounded-lg shadow-lg overflow-hidden flex-1">
             @include('sections.location-section', ['style' => 'card'])
         </div>
+
+        {{-- Card inferior: Áreas de Servicio --}}
+        <div class="flex flex-col">
+           @include('sections.contact-section')
+        </div>
     </div>
+
 
 </main>
 @endsection
