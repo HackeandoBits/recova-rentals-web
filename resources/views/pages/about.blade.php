2272fa0f34d199a29fe80024267351a4da5c4ca9
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-11-02 22:33:48 -0300

    feat:ordena un grid para contener los componentes de la vista

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,22 +4,27 @@
 @section('content')
 <main class="pt-14 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mb-8">
 
-    {{-- AboutSection: título o componente futuro --}}
-    <div class="mb-8">
+       <div class="mb-8">
         @include('sections.about-section')
     </div>
-<div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-    {{-- Columna izquierda --}}
-    <div class="flex flex-col gap-5">
-        @include('sections.history-section', ['style' => 'card'])
-        
-    </div>
+@php
+$tarjetas = [
+    ['col' => 'left', 'view' => 'sections.history-section', 'data' => ['style' => 'card']],
+    ['col' => 'right', 'view' => 'sections.distinction-section', 'data' => ['style' => 'card']],
+    ['col' => 'left', 'view' => 'sections.history-section', 'data' => ['style' => 'card']],
+    ['col' => 'right', 'view' => 'sections.distinction-section', 'data' => ['style' => 'card']],
+    // más tarjetas...
+];
+@endphp
 
-    {{-- Columna derecha --}}
-    <div class="flex flex-col gap-5">
-      @include('sections.distinction-section', ['style' => 'card'])
-    </div>
+<div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+    @foreach($tarjetas as $tarjeta)
+       
+            @include($tarjeta['view'], $tarjeta['data'])
+        
+    @endforeach
 </div>
+
 
 
 </main>
