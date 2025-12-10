09d6ade8a7a18a4fac36f2985ddecffe54124e48
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-12-10 04:56:53 -0300

    config: configura php.ini, strtup.sh y ngnix.comf

--- startup.sh
+++ startup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
+
+# Copiar configuración de Nginx
+cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-enabled/default
+
+# Reiniciar Nginx
 service nginx reload
+
+# Ejecutar migraciones
+php /home/site/wwwroot/artisan migrate --force
+
+# Limpiar y cachear
+php /home/site/wwwroot/artisan config:cache
+php /home/site/wwwroot/artisan route:cache
+php /home/site/wwwroot/artisan view:cache
+
+cd /home/site/wwwroot && npm ci
+npm run build
+
+php /home/site/wwwroot/artisan storage:link
